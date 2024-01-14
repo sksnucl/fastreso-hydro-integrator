f508d9fdb85aaca7108c5b10e64191b4f44bcb32
Sushant Kumar Singh <115153427+sksnucl@users.noreply.github.com>
2024-01-15 01:13:16 +0530

    Create freezeout.h

---
+++ freezeout.h
@@ -0,0 +1,70 @@
+#ifndef FREEZEOUT_H
+#define FREEZEOUT_H
+
+#include <vector>
+#include <string>
+#include <stdexcept>
+#include <array>
+#include "particle.h"
+
+struct FourVector {
+  double tau, x, y, eta ;
+  
+  double getComponent(int i) const {
+    switch (i) {
+    case 0: return tau;
+    case 1: return x;
+    case 2: return y;
+    case 3: return eta;
+    default: 
+      throw std::out_of_range("Wrong vector index");
+    }
+  }
+};
+
+struct SymmetricTensor {
+  std::array<double, 10> element;
+  
+  double getComponent(int i, int j) {
+    if (i < 0 || i > 3 || j < 0 || j > 3) {
+      throw std::out_of_range("Invalid indices in symmmetric tensor");
+    }
+    
+    if (i > j) {
+      std::swap(i, j);
+    }
+    
+    return element[i * (7 - i) / 2 + j];
+  }
+};
+
+struct surface_element{
+  FourVector xmu;
+  FourVector dSigma;
+  FourVector umu;
+  double temperature;
+  double energy;
+  double entropy;
+  double muB;
+  double muS;
+  double muQ;
+  double bulk;
+  SymmetricTensor shear;
+};
+
+class Freezeout {
+public:
+  Freezeout(const std::string& filename);
+  //~Freezeout();
+  
+  std::vector<surface_element> mysurface;
+  size_t NumberofCells;
+  
+  double EdNd3p(const Particle& particle, double pT, double y, double phi, int eps);
+  
+private:
+  std::string filename_;
+  void readFile(const std::string& dataFile);
+};
+
+#endif
