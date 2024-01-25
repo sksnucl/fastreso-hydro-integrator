707e599ccba9e1f38bf451320896da7a822c9cae
Sushant Kumar Singh <115153427+sksnucl@users.noreply.github.com>
2024-01-25 14:57:58 +0100

    Update particle.h

--- particle.h
+++ particle.h
@@ -8,51 +8,64 @@
 
 class Particle {
 public:
-  Particle(const std::string& name, double mass, int strangeness, int charmness, int baryonNumber, int charge, int isospin3, int spin);
-  ~Particle();
+  //Particle();
+  //~Particle();
   
   std::string getName() const;
   double getMass() const;
   int getStrangeness() const;
   int getCharmness() const;
+  int getBottomness() const;
   int getBaryonNumber() const;
   int getCharge() const;
-  int getSpin() const;
-  int getIsospin3() const;
-  void readf1f2(size_t Ntemp,size_t Npbar); 
+  int getPartId() const;
+  int getDegen() const;
+  double getIsospin3() const;
+  
+  void setName(std::string& name);
+  void setMass(double mass);
+  void setStrangeness(int strangeness);
+  void setCharmness(int charmness);
+  void setBottomness(int bottomness);
+  void setBaryonNumber(int baryonNumber);
+  void setCharge(int charge);
+  void setPartId(int pid);
+  void setDegen(int degen);
+  void setIsospin3(double isospin);
+  
+  void read_fastreso_components(const std::string& folderpath, const std::vector<double>& temparr, const std::vector<double>& pbararr);
+  void clear();
   
   double interpolated_f1(double temp_value, double pbar_value) const;
   double interpolated_f2(double temp_value, double pbar_value) const;
   
 private:
   std::string name_;
+  int pid_;
   double mass_;
   int strangeness_;
   int charmness_;
   int baryonNumber_;
   int charge_;
-  int isospin3_;
-  int spin_;
+  double isospin3_;
+  int bottomness_;
   int degen_;
+  int partid_;
   
-  std::vector<double>* temp;   // Pointer to dynamically allocated vector
-  std::vector<double>* pbar;   // Pointer to dynamically allocated vector
-  std::vector<std::vector<double> >* f1;
-  std::vector<std::vector<double> >* f2;
+  std::vector<double> temp;
+  std::vector<double> pbar;
+  std::vector<std::vector<double> > f1;
+  std::vector<std::vector<double> > f2;
   
   const gsl_interp2d_type *T = gsl_interp2d_bilinear;
-  double *zaf1;
-  double *zaf2;
   gsl_spline2d *splinef1 ;
   gsl_spline2d *splinef2 ;
   gsl_interp_accel *xacc ;
   gsl_interp_accel *yacc ;
-  size_t nx;
-  size_t ny;
   double maxpstar;
   double minpstar;
-  const double mintemp = 0.11;
-  const double maxtemp = 0.16;
+  double mintemp;
+  double maxtemp;
 };
 
 #endif
