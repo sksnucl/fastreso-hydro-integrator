ea7f584ddd44b94cea46f8db4206a6e0087eea44
Sushant Kumar Singh <115153427+sksnucl@users.noreply.github.com>
2024-02-15 14:08:49 +0100

    Update particle.cpp

--- particle.cpp
+++ particle.cpp
@@ -4,8 +4,6 @@
 #include <sstream>
 #include <iomanip>
 #include <algorithm>
-#include <gsl/gsl_interp2d.h>
-#include <gsl/gsl_spline2d.h>
 
 void Particle::setName(std::string& name) {
   name_ = name;
@@ -83,27 +81,10 @@ void Particle::setIsospin3(double isospin3) {
   isospin3_ = isospin3;
 }
 
-double Particle::getIsospin3() const {
-  return isospin3_;
-}
-
-void Particle::read_fastreso_components(const std::string& folderpath, const std::vector<double>& temparr, const std::vector<double>& pbararr) {
+void Particle::read_fastreso_components(const size_t& decayflag, const std::string& folderpath, const std::vector<double>& temparr, const std::vector<double>& pbararr) {
   
   size_t Ntemp = temparr.size() ;
   size_t Npbar = pbararr.size() ;
-  
-  temp.resize(Ntemp) ;
-  pbar.resize(Npbar) ;
-  
-  temp = temparr;
-  pbar = pbararr;
-  
-  std::sort(temp.begin(), temp.end());
-  
-  mintemp = temp[0];
-  maxtemp = temp[Ntemp-1];
-  minpstar = pbar[0];
-  maxpstar = pbar[Npbar - 1];
   
   f1.resize(Ntemp, std::vector<double>(Npbar, 0.0));
   f2.resize(Ntemp, std::vector<double>(Npbar, 0.0));
@@ -112,16 +93,20 @@ void Particle::read_fastreso_components(const std::string& folderpath, const std
   
   for (size_t i = 0; i < Ntemp; ++i) {    
     std::ostringstream ss;
-    ss << std::fixed << std::setprecision(4) << temp[i];
+    ss << std::fixed << std::setprecision(4) << temparr[i];
     std::string formattedtemperature = ss.str();
     
     filename.str("");
 
-    filename << folderpath + "PDGid_" + std::to_string(pid_) + "_total_T" + formattedtemperature + "_Fj.out";
+    if(decayflag == 0){
+      filename << folderpath + "PDGid_" + std::to_string(pid_) + "_thermal_T" + formattedtemperature + "_Fj.out";
+    }else{
+      filename << folderpath + "PDGid_" + std::to_string(pid_) + "_total_T" + formattedtemperature + "_Fj.out";
+    }
    
     std::ifstream filedata(filename.str());
     
-    // Skip the first three lines
+    // Skip the first three lines of datafiles
     for (int line = 0; line < 3; ++line) {
       std::string dummy_line;
       getline(filedata, dummy_line);
@@ -134,56 +119,9 @@ void Particle::read_fastreso_components(const std::string& folderpath, const std
       
       getline(filedata, line_data);
       std::istringstream iss(line_data);
-      int j = line-3;
-      iss >> dummypbar >> dummymass >> f1[i][j] >> f2[i][j];
+      iss >> dummypbar >> dummymass >> f1[i][line] >> f2[i][line];
     }
     filedata.close();
   }
   
-  size_t nx = Ntemp; // number of files
-  size_t ny = Npbar; // number of lines in file
-  
-  // Allocate memory for arrays
-  double zaf1[Ntemp * Npbar];
-  double zaf2[Ntemp * Npbar];
-      
-  // GSL Interpolation setup
-  splinef1 = gsl_spline2d_alloc(T, nx, ny);
-  splinef2 = gsl_spline2d_alloc(T, nx, ny);
-  xacc = gsl_interp_accel_alloc();
-  yacc = gsl_interp_accel_alloc();
-  
-  // Set GSL Interpolation data
-  for (size_t i = 0; i < nx; ++i) {
-    for (size_t j = 0; j < ny; ++j) {
-      gsl_spline2d_set(splinef1, zaf1, i, j, f1[i][j]);
-      gsl_spline2d_set(splinef2, zaf2, i, j, f2[i][j]);
-    }
-  }
-  
-  gsl_spline2d_init(splinef1, temp.data(), pbar.data(), zaf1, nx, ny);
-  gsl_spline2d_init(splinef2, temp.data(), pbar.data(), zaf2, nx, ny);
-}
-
-void Particle::clear(){
-  gsl_spline2d_free(splinef1);
-  gsl_spline2d_free(splinef2);
-  gsl_interp_accel_free(xacc);
-  gsl_interp_accel_free(yacc);
-}
-
-double Particle::interpolated_f1(double temp_value, double pbar_value) const {
-  if (temp_value < mintemp || temp_value > maxtemp || pbar_value < minpstar || pbar_value > maxpstar) {
-    return 0.0;
-  }
-  
-  return gsl_spline2d_eval(splinef1, temp_value, pbar_value, xacc, yacc);
-}
-
-double Particle::interpolated_f2(double temp_value, double pbar_value) const {
-  if (temp_value < mintemp || temp_value > maxtemp || pbar_value < minpstar || pbar_value > maxpstar) {
-    return 0.0;
-  }
-  
-  return gsl_spline2d_eval(splinef2, temp_value, pbar_value, xacc, yacc);
 }
