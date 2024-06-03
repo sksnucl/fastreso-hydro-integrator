3acf17f0dd0d8af009fea2a04ac11386b54d2b61
Sushant Kumar Singh <115153427+sksnucl@users.noreply.github.com>
2024-06-04 02:46:05 +0530

    Update fastreso.h

--- fastreso.h
+++ fastreso.h
@@ -21,14 +21,14 @@ private:
   const Freezeout& freezeout_;
   
   // Define the range for pT, phi, and y
-  double pT_min = 0.0, pT_max = 3.0;
+  double pT_min = 0.1, pT_max = 3.1;
   double phi_min = 0.0, phi_max = 2 * M_PI;
-  double y_min = -1.0, y_max = 1.0;
+  double y_min = -5.0, y_max = 5.0;
   
   // Specify the number of points in each dimension
   const size_t NpT = 21;  /* Number of points in pT */
   const size_t Nphi = 21; /* Number of points in phi */
-  const size_t Ny = 21; /* Number of points in y */
+  const size_t Ny = 51; /* Number of points in y */
   
   double deltapT, deltaphi, deltay;
   
@@ -46,6 +46,7 @@ private:
   std::vector<double> dNdy;
   
   double calc_EdNd3p(const double& pT, const double& y, const double& phi, gsl_spline2d *splinef1, gsl_spline2d *splinef2, gsl_interp_accel *xacc, gsl_interp_accel *yacc, const double& mint, const double& maxt, const double& minp, const double& maxp);
+  double calc_EdNd3p_cart(const double& pT, const double& y, const double& phi, gsl_spline2d *splinef1, gsl_spline2d *splinef2, gsl_interp_accel *xacc, gsl_interp_accel *yacc, const double& mint, const double& maxt, const double& minp, const double& maxp);
   void calc_dNpTdpTdy();
   void calc_dNdy();
   void calc_vn();
